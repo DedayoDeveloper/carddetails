82f077c7f77011f5cc21cb5b2ec6bed1beb24633
Dedayodeveloper <olaleye.dedayo@gmail.com>
2020-08-26 19:49:42 +0100

    project commit
    Signed-off-by: Dedayodeveloper <olaleye.dedayo@gmail.com>

---
+++ src/main/java/com/project/carddetails/CarddetailsApplication.java
@@ -0,0 +1,13 @@
+package com.project.carddetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarddetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarddetailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/carddetails/CarddetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.carddetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarddetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
