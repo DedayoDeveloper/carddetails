9ae04523a70c2b6b39dafcc612c80b1d0b10834f
Dedayodeveloper <olaleye.dedayo@gmail.com>
2020-08-26 23:48:40 +0100

    project commit
    Signed-off-by: Dedayodeveloper <olaleye.dedayo@gmail.com>

--- src/main/java/com/project/carddetails/controllers/WebController.java
+++ src/main/java/com/project/carddetails/controllers/WebController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WebController {
 
-    @GetMapping("/index")
-    @ResponseBody
+    @GetMapping("/")
     public String index(){
         return "index";
     }
